ba378f13961aeb249d5fe4e714a821c48d03024b
MahmoodElbadri <mahmoud.elbadry357@gmail.comgit config --global user.email mahmoud.elbadry357@gmail.comgit config --global user.email mahmoud.elbadry357@gmail.com>
2025-01-24 20:10:10 +0200

    Add project files.

---
+++ ResortWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResortWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
