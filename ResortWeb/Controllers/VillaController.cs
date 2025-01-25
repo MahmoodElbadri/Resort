8fb95fc627ffb099d3b7d652fa171402bdfd3b7a
MahmoodElbadri <mahmoud.elbadry357@gmail.comgit config --global user.email mahmoud.elbadry357@gmail.comgit config --global user.email mahmoud.elbadry357@gmail.com>
2025-01-25 14:56:05 +0200

    Villa Crud Operations

--- ResortWeb/Controllers/VillaController.cs
+++ ResortWeb/Controllers/VillaController.cs
@@ -26,9 +26,59 @@ namespace ResortWeb.Controllers
         [HttpPost]
         public IActionResult Create(Villa villa)
         {
-            _db.Add(villa);
-            _db.SaveChanges();
-            return RedirectToAction("Index","Villa");
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("", "Name and Description should be different");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Add(villa);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(villa);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(x => x.Id == id);
+            if (villa == null) { return RedirectToAction("Error","Home"); }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa villa)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Update(villa);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(villa);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(x => x.Id == id);
+            if (villa == null) { return RedirectToAction("Error", "Home"); }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            var existVilla = _db.Villas.FirstOrDefault(x => x.Id == villa.Id);
+            if (existVilla is not null)
+            {
+                _db.Villas.Remove(existVilla);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
         }
     }
 }
