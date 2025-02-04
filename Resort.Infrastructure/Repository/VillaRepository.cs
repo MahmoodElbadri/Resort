d72638ec0d84eec3d0696035f689348228402cf4
mahmoud salah <mahmoudsalah55@hotmail.com>
2025-02-04 22:40:32 +0200

    setting dependencies

---
+++ Resort.Infrastructure/Repository/VillaRepository.cs
@@ -0,0 +1,43 @@
+﻿using Resort.Application.Common.Interfaces;
+using Resort.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort.Infrastructure.Repository;
+
+public class VillaRepository : IVillaRepository
+{
+    public void Add(Villa entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Villa> Get(Expression<Func<IVillaRepository, bool>> predicate, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Villa> GetAll(Expression<Func<IVillaRepository, bool>>? predicate = null, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Villa entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Villa entity)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Resort.Web/Program.cs
+++ Resort.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Resort.Application.Common.Interfaces;
 using Resort.Infrastructure.Data;
+using Resort.Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IVillaRepository,VillaRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
