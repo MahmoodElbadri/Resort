f301a95868b48eff52199d5c7cb6b2ca26fe96b2
mahmoud salah <mahmoud.elbadry357@gmail.com>
2025-02-04 23:19:21 +0200

    Add generic repository interface and implementation
    Introduce IRepository<T> for common data operations in Resort.Application.Common.Interfaces.
    Add Repository<T> class in Resort.Infrastructure.Repository with methods throwing NotImplementedException.
    Include necessary using directives.

---
+++ Resort.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resort.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort.Application.Common.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<Villa> GetAll
+            (Expression<Func<Villa, bool>>? predicate = null, string? includeProperties = null);
+        Villa Get
+            (Expression<Func<Villa, bool>> predicate, string? includeProperties = null);
+        void Add(Villa entity);
+        void Remove(Villa entity);
+    }
+}
---
+++ Resort.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Resort.Application.Common.Interfaces;
+using Resort.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Villa Get(Expression<Func<Villa, bool>> predicate, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? predicate = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
