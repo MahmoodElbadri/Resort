a0f4ca95f7035c2ac71c9f436469ac5d13fa34e8
mahmoud salah <mahmoudsalah55@hotmail.com>
2025-02-04 22:33:43 +0200

    Add IVillaRepository and project reference to Domain
    - Added `IVillaRepository` interface in `Resort.Application.Common.Interfaces` for managing `Villa` entities.

---
+++ Resort.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Resort.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resort.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villa> GetAll
+            (Expression<Func<IVillaRepository,bool>>? predicate = null,string? includeProperties = null);
+        IEnumerable<Villa> Get
+            (Expression<Func<IVillaRepository,bool>> predicate,string? includeProperties = null);
+        void Add(Villa entity);
+        void Update(Villa entity);
+        void Remove(Villa entity);
+        void Save();
+    }
+}
