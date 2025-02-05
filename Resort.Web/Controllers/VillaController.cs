9092f597ba4d588a0a510eb30c459371f15dbcee
mahmoud salah <mahmoud.elbadry357@gmail.com>
2025-02-05 20:09:47 +0200

    Refactor VillaController to use IUnitOfWork
    - Changed namespace declaration from curly braces to semicolon.
    - Replaced `IVillaRepository` with `IUnitOfWork` in `VillaController`.
    - Updated `VillaController` constructor to accept `IUnitOfWork`.
    - Modified `Index` action to use `_unitOfWork.Villa.GetAll()`.
    - Updated `Create` actions (GET and POST) to use `_unitOfWork.Villa`.
    - Updated `Update` actions (GET and POST) to use `_unitOfWork.Villa`.
    - Updated `Delete` actions (GET and POST) to use `_unitOfWork.Villa`.
    - Retained error handling and success messages with `IUnitOfWork`.

--- Resort.Web/Controllers/VillaController.cs
+++ Resort.Web/Controllers/VillaController.cs
@@ -4,90 +4,89 @@ using Resort.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Resort.Application.Common.Interfaces;
 
-namespace Resort.Web.Controllers
-{
-    public class VillaController : Controller
-    {   
-        private readonly IVillaRepository _villaRepo;
+namespace Resort.Web.Controllers;
 
-        public VillaController(IVillaRepository villaRepo)
-        {
-            this._villaRepo = villaRepo;
-        }
-        public IActionResult Index()
-        {
-            var villas = _villaRepo.GetAll();
-            return View(villas);
-        }
+public class VillaController : Controller
+{   
+    private readonly IUnitOfWork _unitOfWork;
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(Villa villa)
-        {
-            if (ModelState.IsValid)
-            {
-                _villaRepo.Add(villa);
-                _villaRepo.Save();
-                TempData["success"] = "Villa created successfully";
-                return RedirectToAction("Index");
-            }
-            TempData["error"] = "Villa has not been created";
-            return View(villa); 
-        }
+    public VillaController(IUnitOfWork unitOfWork)
+    {
+        this._unitOfWork = unitOfWork;
+    }
+    public IActionResult Index()
+    {
+        var villas = _unitOfWork.Villa.GetAll();
+        return View(villas);
+    }
 
-        [HttpGet]
-        public IActionResult Update(int id)
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Villa villa)
+    {
+        if (ModelState.IsValid)
         {
-            var villa = _villaRepo.Get(v => v.Id == id);
-            if(villa == null)
-            {
-                return RedirectToAction("Error","Home");
-            }
-            return View(villa);
+            _unitOfWork.Villa.Add(villa);
+            _unitOfWork.Villa.Save();
+            TempData["success"] = "Villa created successfully";
+            return RedirectToAction("Index");
         }
+        TempData["error"] = "Villa has not been created";
+        return View(villa); 
+    }
 
-        [HttpPost]
-        public IActionResult Update(Villa villa)
+    [HttpGet]
+    public IActionResult Update(int id)
+    {
+        var villa = _unitOfWork.Villa.Get(v => v.Id == id);
+        if(villa == null)
         {
-            if (ModelState.IsValid)
-            {
-                _villaRepo.Update(villa);
-                _villaRepo.Save();
-                TempData["success"] = "Villa updated successfully";
-                return RedirectToAction("Index");
-            }
-            TempData["error"] = "Villa has not been updated";
-            return View(villa);
+            return RedirectToAction("Error","Home");
         }
+        return View(villa);
+    }
 
-        [HttpGet]
-        public IActionResult Delete(int id)
+    [HttpPost]
+    public IActionResult Update(Villa villa)
+    {
+        if (ModelState.IsValid)
         {
-            var villa = _villaRepo.Get  (v => v.Id == id);
-            if (villa == null)
-            {
-                return RedirectToAction("Error", "Home");
-            }
-            return View(villa);
+            _unitOfWork.Villa.Update(villa);
+            _unitOfWork.Villa.Save();
+            TempData["success"] = "Villa updated successfully";
+            return RedirectToAction("Index");
         }
+        TempData["error"] = "Villa has not been updated";
+        return View(villa);
+    }
 
-        [HttpPost]
-        public IActionResult Delete(Villa villa)
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var villa = _unitOfWork.Villa.Get  (v => v.Id == id);
+        if (villa == null)
         {
-            var existedVilla = _villaRepo.Get(v => v.Id == villa.Id);
-            if (existedVilla is not null)
-            {
-                _villaRepo.Remove(existedVilla);
-                _villaRepo.Save();
-                TempData["success"] = "Villa deleted successfully";
-                return RedirectToAction("Index");
-            }
-            TempData["error"] = "Villa has not been deleted";
-            return View();
+            return RedirectToAction("Error", "Home");
         }
+        return View(villa);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Villa villa)
+    {
+        var existedVilla = _unitOfWork.Villa.Get(v => v.Id == villa.Id);
+        if (existedVilla is not null)
+        {
+            _unitOfWork.Villa.Remove(existedVilla);
+            _unitOfWork.Villa.Save();
+            TempData["success"] = "Villa deleted successfully";
+            return RedirectToAction("Index");
+        }
+        TempData["error"] = "Villa has not been deleted";
+        return View();
     }
 }
