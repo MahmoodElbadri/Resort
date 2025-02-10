f1880af7c8da40930d5df9af2b08da8dc8cf8818
mahmoud salah <mahmoud.elbadry357@gmail.com>
2025-02-10 19:26:59 +0200

    Add Amenity repository and update UnitOfWork
    Updated IUnitOfWork interface to include IAmenityRepository property.
    Implemented IAmenityRepository in UnitOfWork and initialized it with AmenityRepository.
    Removed Save method from VillaNumberRepository.
    Created IAmenityRepository interface with Update method.
    Added AmenityRepository class implementing IAmenityRepository.

--- Resort.Infrastructure/Repository/AmenityRepository.cs
+++ Resort.Infrastructure/Repository/AmenityRepository.cs
@@ -4,22 +4,23 @@ using Resort.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Resort.Infrastructure.Repository
 {
-    public class VillaNumberRepository: Repository<VillaNumber>, IVillaNumberRepository
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
     {
         private readonly ApplicationDbContext _db;
-        public VillaNumberRepository(ApplicationDbContext db):base(db)
+        public AmenityRepository(ApplicationDbContext db) : base(db)
         {
-            this._db = db;
+            _db = db;
         }
 
-        public void Update(VillaNumber entity)
+        public void Update(Amenity amenity)
         {
-            _db.Update(entity);
+            _db.Update(amenity);
         }
     }
 }
