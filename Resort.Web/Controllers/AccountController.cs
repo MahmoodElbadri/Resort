2ebeb4be5de672a262335ec6065c950e66ea76fc
mahmoud salah <mahmoud.elbadry357@gmail.com>
2025-02-15 12:19:00 +0200

    Add AccountController and update _LoginPartial links
    - Modified `_LoginPartial.cshtml` to include `asp-action` and `asp-controller` attributes for "Register" and "Login" links, linking them to the `Register` and `Login` actions in the `AccountController`.
    - Added a new `AccountController` class in `AccountController.cs` with dependencies on `IUnitOfWork`, `UserManager<ApplicationUser>`, `SignInManager<ApplicationUser>`, and `RoleManager<IdentityRole>`.
    - Implemented `Login` and `Register` actions in `AccountController` to return their respective views.

---
+++ Resort.Web/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Resort.Application.Common.Interfaces;
+using Resort.Domain.Entities;
+
+namespace Resort.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AccountController(IUnitOfWork unitOfWork,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            this.unitOfWork = unitOfWork;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
