7aaccf044fab0a8721f722773822121a5f088f59
mahmoud salah <mahmoud.elbadry357@gmail.com>
2025-02-17 21:05:56 +0200

    Add FinalizeBooking and BookingConfirmation actions
    Introduce new actions in BookingController: FinalizeBooking and BookingConfirmation. FinalizeBooking is a POST method that calculates the total cost, sets the booking status to pending, records the booking date, adds the booking to the database, and redirects to BookingConfirmation. BookingConfirmation returns a view with the booking ID. Both actions require authorization. Also, add a new using directive for Resort.Application.Utility.

--- Resort.Web/Controllers/BookingController.cs
+++ Resort.Web/Controllers/BookingController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Resort.Application.Common.Interfaces;
+using Resort.Application.Utility;
 using Resort.Domain.Entities;
 using System.Security.Claims;
 
@@ -36,5 +37,25 @@ public class BookingController : Controller
         };
         booking.TotalCost = booking.Villa.Price * nights;
         return View(booking);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public IActionResult FinalizeBooking(Booking booking)
+    {
+        var villa = _unitOfWork.Villa.Get(tmp => tmp.Id == booking.VillaId);
+        
+        booking.TotalCost = booking.Villa.Price * booking.Nights;
+        booking.Status = SD.StatusPending;
+        booking.BookingDate = DateTime.Now;
+        _unitOfWork.Booking.Add(booking);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(BookingConfirmation), new {bookingId = booking.Id});
+    }
+
+    [Authorize]
+    public  IActionResult BookingConfirmation(int bookingId)
+    {
+        return View(bookingId);
     }
 }
