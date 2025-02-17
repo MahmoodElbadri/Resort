ee3df0488b13523e98faa007a14a93d58a983c3c
mahmoud salah <mahmoud.elbadry357@gmail.com>
2025-02-17 20:06:33 +0200

    Add booking functionality with new controller and views
    Modified Index.cshtml to add id attribute to select element.
    Updated _VillaList.cshtml to include asp-action link for booking.
    Created BookingController.cs with FinalizeBooking action.
    Added FinalizeBooking.cshtml to display booking details and form.

---
+++ Resort.Web/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resort.Application.Common.Interfaces;
+using Resort.Domain.Entities;
+
+namespace Resort.Web.Controllers;
+
+public class BookingController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public BookingController(IUnitOfWork unitOfWork)
+    {
+        this._unitOfWork = unitOfWork;
+    }
+    public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate,int nights)
+    {
+        var booking = new Booking
+        {
+            VillaId = villaId,
+            Villa = _unitOfWork.Villa.Get(tmp=>tmp.Id==villaId,includeProperties:"VillaAmenity"),
+            CheckInDate = checkInDate,
+            Nights = nights,
+            CheckOutDate = checkInDate.AddDays(nights)
+        };
+        return View(booking);
+    }
+}
