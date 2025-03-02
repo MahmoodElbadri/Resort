a1f73ad1e3c85068eef9f2d0c6e87a761eabdb60
mahmoud salah <mahmoud.elbadry357@gmail.com>
2025-03-03 00:08:38 +0200

    Update BookingController and _Layout.cshtml for new features
    - Added villa availability check in BookingController before finalizing bookings.
    - Replaced navigation bar in _Layout.cshtml with a new structure.
    - Added conditional dropdown for Admin role and "Manage Bookings" link for non-admin users.
    - Included a partial view for login functionality.
    - Improved navigation bar responsiveness using Bootstrap's collapse feature.

--- Resort.Web/Controllers/BookingController.cs
+++ Resort.Web/Controllers/BookingController.cs
@@ -57,6 +57,18 @@ public class BookingController : Controller
         booking.Status = SD.StatusPending;
         booking.BookingDate = DateTime.Now;
 
+        var villaNumbersList = _unitOfWork.VillaNumber.GetAll().ToList();
+        var bookedVillas = _unitOfWork.Booking.GetAll
+            (tmp => tmp.Status == SD.StatusApproved || tmp.Status == SD.StatusCheckedIn).ToList();
+            int roomAvailable =
+                SD.VillaRoomsAvailable_Count(villa.Id, villaNumbersList, booking.CheckInDate, booking.Nights, bookedVillas);
+            
+        if(roomAvailable == 0)
+        {
+            return RedirectToAction(nameof(FinalizeBooking), 
+                new { villaId = booking.VillaId, checkInDate = booking.CheckInDate, nights = booking.Nights });
+        }
+
         _unitOfWork.Booking.Add(booking);
         _unitOfWork.Save();
 
